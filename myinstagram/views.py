96cbe3302ee57129b19d846403519dfe425935b9
Moses-254-Mugo <moses.mugo@student.moringaschool.com>
2021-10-16 00:45:32 +0300

    Create search view function

--- myinstagram/views.py
+++ myinstagram/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
 from django.http  import HttpResponse
 
+from myinstagram.models import Images
+
 
 
 # Create your views here.
 def welcome(request):
     return render(request, 'welcome.html')
+
+def search_results(request):
+
+    if 'article' in request.GET and request.GET["article"]:
+        search_term = request.GET.get("article")
+        searched_articles = Images.search_by_title(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'all-news/search.html',{"message":message,"articles": searched_articles})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'all-news/search.html',{"message":message})
