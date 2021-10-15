6b9cd51bab07ff74781dbbc930a6961750c16c74
Moses-254-Mugo <moses.mugo@student.moringaschool.com>
2021-10-15 23:43:29 +0300

    Add tesging instance for profile

--- myinstagram/tests.py
+++ myinstagram/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from .models import Profile, Images
 
 # Create your tests here.
+class ProfileTestClass(TestCase):
+
+    # Set up method
+    def setUp(self):
+        self.moses = Profile(name = "moses")
+
+    # Testing instance 
+    def test_instance(self):
+        self.assertTrue(isinstance(self.moses,Profile))
+
+
