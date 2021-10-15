29478d018e381a1c2a7c1b95dacd8eee78bdfb3d
Moses-254-Mugo <moses.mugo@student.moringaschool.com>
2021-10-16 00:08:15 +0300

    Add models to administraton page

--- myinstagram/models.py
+++ myinstagram/models.py
@@ -20,7 +20,7 @@ class Profile (models.Model):
     @receiver(post_save, sender=User)
     def create_profile(sender, instance, created, **kwargs):
         if created:
-            Profile.object.create(user= instance)
+            Profile.objects.create(user= instance)
 
     @receiver(post_save, sender=User)
     def save_user_profile(sender, instance, **kwargs):
