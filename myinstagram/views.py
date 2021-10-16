c5508daf54629e638d584dd441df5a1501613c28
Moses-254-Mugo <moses.mugo@student.moringaschool.com>
2021-10-16 23:55:52 +0300

    Add route profile

--- myinstagram/views.py
+++ myinstagram/views.py
@@ -1,7 +1,6 @@
 from django.contrib.auth import authenticate
 from django.http.response import HttpResponseRedirect
 from django.shortcuts import redirect, render
-from django.http  import HttpResponse
 from myinstagram.models import Images
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
@@ -35,6 +34,26 @@ def Home(request):
         }
         return render(request, 'Home.html', params)
 
+@login_required(login_url='login')
+def profile(request, username):
+    pictures = request.user.profile.images.all()
+    if request.method == 'POST':
+        userform = MyinstagramUpdateUserForm(request.POST, instance=request.user)
+        profileform = MyinstagramUpdateUserProfileForm(request.POST, request.FILES, instance=request.user.profile)
+        if userform.is_valid() and profileform.is_valid():
+            userform.save()
+            profileform.save()
+
+            return HttpResponseRedirect(request.path_info)
+        else:
+            userform = MyinstagramUpdateUserForm(instance=request.user)
+            profileform = MyinstagramUpdateUserProfileForm(instance=request.user.profile)
+        params = {
+            'userform': userform,
+            'profileform': profileform,
+            'pictures': pictures,
+        }
+        return render(request, 'profile.html', params)
 
 
 
