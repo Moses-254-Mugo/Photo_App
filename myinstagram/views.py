675e15519d4dc679a1ccf043d7c7ca3d330467dd
Moses-254-Mugo <moses.mugo@student.moringaschool.com>
2021-10-16 22:09:33 +0300

    Add @login_required decorator

--- myinstagram/views.py
+++ myinstagram/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from django.http  import HttpResponse
-
 from myinstagram.models import Images
+from django.contrib.auth.decorators import login_required
 
 
 
 # Create your views here.
+@login_required(login_url='/accounts/login/')
 def welcome(request):
     return render(request, 'welcome.html')
 
