f5350aa23e777e5fc73b3bd4d295ab5bbda596fb
Moses-254-Mugo <moses.mugo@student.moringaschool.com>
2021-10-17 00:21:21 +0300

    Create route user_profile

--- myinstagram/views.py
+++ myinstagram/views.py
@@ -1,7 +1,8 @@
 from django.contrib.auth import authenticate
+from django.http import request
 from django.http.response import HttpResponseRedirect
-from django.shortcuts import redirect, render
-from myinstagram.models import Images
+from django.shortcuts import get_object_or_404, redirect, render
+from myinstagram.models import Follow, Images
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from .forms import MyinstagramImageForm, MyinstagramSignUpForm,MyinstagramCommentsForm, MyinstagramUpdateUserForm, MyinstagramUpdateUserProfileForm
@@ -55,6 +56,30 @@ def profile(request, username):
         }
         return render(request, 'profile.html', params)
 
+
+
+@login_required(login_url='login')
+def user_profile(requset, username):
+    user_profile = get_object_or_404(User, username=username)
+    if request.user == user_profile:
+        return redirect('profile', username=request.user.username)
+    user_image = user_profile.profile.images.all()
+
+    followers = Follow.objects.filter(followed=user_profile.profile)
+    follower_status = None
+    for follower in followers:
+        if request.user.profile == follower.follwer:
+            follower_status = True
+        else:
+            follower_status = False
+    params = {
+        'user_profile': user_profile,
+        'user_image': user_image,
+        'followers': followers,
+        'follower_status': follower_status
+    }
+    print(followers)
+    return render(requset, 'user_profile.html', params)
 
 
 def search_results(request):
