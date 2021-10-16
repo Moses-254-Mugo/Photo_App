ae79774e2d10af2ecb9612881f519026d40943d6
Moses-254-Mugo <moses.mugo@student.moringaschool.com>
2021-10-16 17:17:00 +0300

    Add comments and follow model to admin page

--- myinstagram/admin.py
+++ myinstagram/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Profile, Images
+from .models import Profile, Images, Comments, Follow
 
 # Register your models here.
 admin.site.register(Profile)
 admin.site.register(Images)
+admin.site.register(Comments)
+admin.site.register(Follow)
