a7a84c0bcbe1072cad72e03f686936df40539f78
Moses-254-Mugo <moses.mugo@student.moringaschool.com>
2021-10-19 11:48:26 +0300

    Modify secret key

--- instagramClone/settings.py
+++ instagramClone/settings.py
@@ -26,10 +26,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
+SECRET_KEY = ('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 MODE=config("MODE", default="dev")
-SECRET_KEY = config('SECRET_KEY')
+
 DEBUG = os.environ.get('DEBUG', True)
 # development
 if config('MODE')=="dev":
